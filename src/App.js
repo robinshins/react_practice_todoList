ddbcfdb78e2394775909b259d32eb8077f46b78a
robinshins <robinshins674@gmail.com>
2020-04-29 15:18:11 +0900

    basic setting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Form from './components/Form'
+import TodoListTemplate from './components/ToDoListTemplate'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <TodoListTemplate form = {<Form/>}>
+        템플린 완성
+        </TodoListTemplate>
+    );
+  }
 }
 
 export default App;
